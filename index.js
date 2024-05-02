51f26d39f2b3fb06cdc86da4c78e7154c24892f9
Deo Seth <diothewind@gmail.com>
2024-05-02 20:55:22 +0800

    Implement GET route to get info of amount of persons in the phonebook

--- index.js
+++ index.js
@@ -28,6 +28,11 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/info', (request, response) => {
+    const date = new Date()
+    response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${date}</p>`)
+})
+
 const PORT = 3002
 app.listen(PORT, () => {
     console.log(`Server is listening to PORT ${PORT}...`)
